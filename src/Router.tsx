21842dd1155160d0d8032ccd71182421d5d3288b
Joanderson <joanderson.silva-terceiro@cea.com.br>
2022-06-27 09:50:44 -0300

    rotas

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,12 +1,18 @@
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
+
+import { useGetLessonsQuery } from "./graphql/generated";
 import { Event } from './pages/Event/Event'
 import { Subscribe } from "./pages/Event/Subscribe";
 
 export function Router() {
+
+  const { data } = useGetLessonsQuery();
+
   return (
     <Routes>
-      <Route path="/" element={<Subscribe />}/>
-      <Route path="/event" element={<Event />} />
+      <Route path="/" element={<Subscribe />} />
+      <Route path="/event" element={data && <Navigate replace to={`/event/lesson/${data?.lessons[0]?.slug}`} />} />
+      <Route path="/event/lesson" element={data && <Navigate replace to={`/event/lesson/${data?.lessons[0]?.slug}`} />} />
       <Route path="/event/lesson/:slug" element={<Event />} />
     </Routes>
   )
